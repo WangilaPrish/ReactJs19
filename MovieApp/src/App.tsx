d35a29108b34407dc36d5a0a7ddfbb3e367f7ef3
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-06 23:03:34 +0300

    feat: implement Search component with basic structure

--- MovieApp/src/App.tsx
+++ MovieApp/src/App.tsx
@@ -1,4 +1,4 @@
-
+import Search from './components/Search.js'
 
 const App = () => {
     return (
@@ -6,10 +6,10 @@ const App = () => {
             <div className="pattern" />
             <div className="wrapper">
                 <header>
-                    <img src="./hero.png" alt="Hero Image" />
+                    <img src="./hero.png" alt="Hero Banner" />
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
                 </header>
-                <p>Search</p>
+                <Search />
             </div>
         </main>
 
