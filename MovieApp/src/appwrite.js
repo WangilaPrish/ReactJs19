12ba9cce07a13d958ea423be48f65d21c02dfe19
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-08 21:57:31 +0300

    feat: add getTrendingMovies function to retrieve top trending movies based on search count

--- MovieApp/src/appwrite.js
+++ MovieApp/src/appwrite.js
@@ -36,3 +36,9 @@ export const updateSearchCount = async (searchTerm, movie) => {
    console.error(error);
   }
  }
+ export const getTrendingMovies = async () => {
+    try {
+     const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+       Query.limit(5),
+       Query.orderDesc("count")
+     ])
