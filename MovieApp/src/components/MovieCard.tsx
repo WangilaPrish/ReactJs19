4c133f5978cd8e7bba559bf595a09c97b7c41c1b
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-08 21:53:50 +0300

    feat: complete MovieCard component layout with title and rating display

--- MovieApp/src/components/MovieCard.tsx
+++ MovieApp/src/components/MovieCard.tsx
@@ -10,3 +10,11 @@ const MovieCard = ({ movie:
                     `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
                 alt={title}
             />
+            <div className="mt-4">
+                <h3>{title}</h3>
+
+                <div className="content">
+                    <div className="rating">
+                        <img src="star.svg" alt="Star Icon" />
+                        <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+                    </div>
