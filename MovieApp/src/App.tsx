a8d9b6511dfea11ad80d0ac618da5ad8bebf3692
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-08 21:51:45 +0300

    feat: add useEffect hooks to fetch movies and trending movies on component mount

--- MovieApp/src/App.tsx
+++ MovieApp/src/App.tsx
@@ -73,3 +73,22 @@ const App = () => {
             console.error(`Error fetching trending movies: ${error}`);
         }
     }
+    useEffect(() => {
+        fetchMovies(debouncedSearchTerm);
+    }, [debouncedSearchTerm]);
+
+    useEffect(() => {
+        loadTrendingMovies();
+    }, []);
+
+    return (
+        <main>
+            <div className="pattern" />
+
+            <div className="wrapper">
+                <header>
+                    <img src="./hero.png" alt="Hero Banner" />
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                </header>
